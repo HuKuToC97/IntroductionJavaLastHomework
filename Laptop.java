628272a4d983b525f392205e7df1dadeeb3dbcee
HuKuToC97 <hukuta199701@gmail.com>
2024-02-11 11:18:43 +0300

    I break GIT(3)

--- Laptop.java
+++ Laptop.java
@@ -1,5 +1,14 @@
 package JavaLastHomework;
 
 public class Laptop {
+    String name;
+    Integer amountRAM;
+    Integer capacityHardDrive;
+    String operatingSystem;
+    String color;
     
+    @Override
+    public String toString() {
+        return "Laptop :" + name;
+    }
 }
--- main.java
+++ main.java
@@ -5,7 +5,7 @@ package JavaLastHomework;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("No");
     }
 }
     
